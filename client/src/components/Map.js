f77da75e44ba492d99b681c48fded3a443934953
Mark DeVito <markdv@alumni.unc.edu>
2020-12-04 16:51:04 -0500

    its pink. also a marker on copenhagen

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -5,7 +5,7 @@ function Map(props) {
     const mapContainer = useRef(null)
     
     const styles = {
-        width: "100vw",
+        width: "80vw",
         height: "calc(100vh - 80px)",
         position: "absolute"
       }
@@ -14,10 +14,13 @@ function Map(props) {
         mapboxgl.accessToken = 'pk.eyJ1IjoibWFya2R2IiwiYSI6ImNraWFubmhzbjAxb3IyeWsyODQ2cXBvbmUifQ.huPMP5ZK_GUqsbjHTgXRcw'
         const map = new mapboxgl.Map({
             container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v11',
+            style: 'mapbox://styles/markdv/ckiagvwfa0o8u19qgxqkrjyr2',
             center: [5, 34],
             zoom: 2
-            });
+            })
+        const marker = new mapboxgl.Marker()
+            .setLngLat([12.550343, 55.665957])
+            .addTo(map)
     }, [])
 
     return (
