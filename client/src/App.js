6e8bf5ab7a8fb2a1d7fc06a8c8b3e0128205f023
Mark DeVito <markdv@alumni.unc.edu>
2020-12-05 19:04:46 -0500

    app.js imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect} from 'react'
+import { Switch, Route, withRouter } from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import CreateMemory from './pages/CreateMemory'
+import EditMemory from './pages/EditMemory'
+import Profile from './pages/Profile'
 
 function App() {
+  
    return (
     <div className='App'>
     </div>
    )
  }
- export default App
+ export default withRouter(App)
  
