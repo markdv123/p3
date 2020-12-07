b6d67ba3d0b45efe69782837b7756d1647254b11
Andrew Cruz <andrewcruz@Andrews-MacBook-Air.local>
2020-12-07 15:15:22 -0500

    converting Material-UL into NAV

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,26 +1,55 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { AppBar } from '@material-ui/core'
+import { Toolbar } from '@material-ui/core'
+import { IconButton } from '@material-ui/core'
+import { MenuIcon } from '@material-ui/icons'
+import { Typography } from '@material-ui/core'
+import { Button } from '@material-ui/core'
 
-export default ({authenticated, currentUser}) => {
+
+
+
+
+export default ({ authenticated, currentUser }) => {
     return authenticated && currentUser ? (
-        <nav>
-            <div className="nav-wrapper">
-                <Link to="/" className="brand-logo"><i className="material-icons" style={{"marginLeft": "10px"}}>public</i>Title</Link>
-                <ul id='nav-mobile' className="right hide-on-med-and-down">
-                    <li><Link to="/profile">Profile</Link></li>
-                    <li><Link to="/" onClick={() => localStorage.clear()}>Signout</Link></li>
-                </ul>
-            </div>
-        </nav>
+        // <nav>
+        //     <div className="nav-wrapper">
+        //         <Link to="/" className="brand-logo"><i className="material-icons" style={{"marginLeft": "10px"}}>public</i>Title</Link>
+        //         <ul id='nav-mobile' className="right hide-on-med-and-down">
+        //             <li><Link to="/profile">Profile</Link></li>
+        //             <li><Link to="/" onClick={() => localStorage.clear()}>Signout</Link></li>
+        //         </ul>
+        //     </div>
+        // </nav>
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start"  color="inherit" aria-label="menu">
+                        {/* <MenuIcon /> */}
+                    </IconButton>
+                    <Typography variant="h6" >
+                        Title
+                    </Typography>
+                    <Button color="inherit" href='/profile' >Profile</Button>
+                    <Button color="inherit" href='/' onClick={() => localStorage.clear()} >Logout</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
     ) : (
-        <nav>
-            <div className="nav-wrapper">
-                <Link to="/" className="brand-logo"><i className="material-icons" style={{"marginLeft": "10px"}}>public</i>Title</Link>
-                <ul id='nav-mobile' className="right hide-on-med-and-down">
-                    <li><Link to="/register">Sign Up</Link></li>
-                    <li><Link to="/login">Sign In</Link></li>
-                </ul>
-            </div>
-        </nav>
-    )
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start"  color="inherit" aria-label="menu">
+                        {/* <MenuIcon /> */}
+                    </IconButton>
+                    <Typography variant="h6" style={{flexGrow : 1}}>
+                        Title
+                    </Typography>
+                    <Button color="inherit"  href="/login" >Sign In</Button>
+                    <Button color="inherit" href='/register'>Sign Up</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+        )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,13 +9,13 @@ const Home = (props) => {
             <div style={{textAlign:"center"}}>
                 <h1>Welcome to [title redacted]</h1>
                 <p style={{maxWidth:"820px", margin: "0 auto"}}>[title redacted] is a wonderful new app designed to help users visually keep track of precious memories on a global map! In your profile page, a personal, interactive map will appear. Click on it to create a new memory pin. Over time, you can see your map fill up with all of the chosen memories from your life!</p>
-                <Link to="/register" style={{margin:"5px"}} className="btn waves-effect waves-light" name="action">Get Started
+                {/* <Link to="/register" style={{margin:"5px"}} className="btn waves-effect waves-light" name="action">Get Started
                     <i className="material-icons left">person_add</i>
                 </Link>
                 <br />
                 <Link to="/login" style={{margin:"5px"}} className="btn waves-effect waves-light" name="action">Sign In
                     <i className="material-icons left">person</i>
-                </Link>
+                </Link> */}
             </div>
         </div>
    )
