4e642a3770f4838d8d8cb5dbc93cb0d1c5da2e23
Mark DeVito <markdv@alumni.unc.edu>
2020-12-07 09:18:49 -0500

    rerendering app in index, created fromend route to map for testing

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
-import Map from './components/Map'
-import Nav from './components/Nav'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Map /> 
+      <App /> 
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
