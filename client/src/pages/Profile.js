526b0b41085320675ae22308eabe927ffb6b4970
Mark DeVito <markdv@alumni.unc.edu>
2020-12-07 16:51:16 -0500

    dammit

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,30 +1,30 @@
-import React, {useState, useEffect} from 'react'
-import {withRouter} from 'react-router-dom'
-import {Grid, Container} from '@material-ui/core'
+import React, { useState, useEffect } from 'react'
+import { withRouter } from 'react-router-dom'
+import { Grid, Container } from '@material-ui/core'
 import Map from '../components/Map'
 import Nav from '../components/Nav'
 import Memory from '../components/Memory'
 
 
-function Profile (props) {
+function Profile(props) {
 
-    return(
+    return (
         <div>
             <Nav
                 authenticated={props.authenticated}
-                currentUser={props.currentUser}/>
-            <Container style={{display: 'flex'}}>
+                currentUser={props.currentUser} />
+            <Container style={{ display: 'flex' }}>
                 <Grid xs={12} sm={6}>
-               <div className='map center'>
-                    <h1>Hello</h1>
-                    <Map {...props}/>
-                </div> 
-            </Grid>
-            <Grid xs={12} sm={6}>
-                <Memory />
-            </Grid>
+                    <div className='map center'>
+                        <h1>Hello</h1>
+                        <Map {...props} />
+                    </div>
+                </Grid>
+                <Grid xs={12} sm={6}>
+                    <Memory />
+                </Grid>
             </Container>
-            
+
         </div>
     )
 }
