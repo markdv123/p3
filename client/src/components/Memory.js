d85d1f23b4da6b26bf9dc409b5bd8207afa18ea5
Mark DeVito <markdv@alumni.unc.edu>
2020-12-08 13:09:53 -0500

    whattttt

--- client/src/components/Memory.js
+++ client/src/components/Memory.js
@@ -1,16 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import { withRouter } from 'react-router-dom'
-import { List, ListItem, Button, Icon } from '@material-ui/core'
+import { List, ListItem, Button, Icon, makeStyles, ListItemText } from '@material-ui/core'
+import { FixedSizeList } from 'react-window'
 import ViewMemory from '../pages/ViewMemory'
 import CreateMemory from '../pages/CreateMemory'
 import EditMemory from '../pages/EditMemory'
 import { __DeleteMemory } from '../services/MemoryService'
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        height: 400,
+        maxWidth: 300,
+        backgroundColor: theme.palette.background.paper,
+    },
+}))
+
 const Memory = (props) => {
     const [mode, setMode] = useState('')
-    console.log('mem', props)
+    const classes = useStyles()
 
-    useEffect(()=> {
+    useEffect(() => {
         setMode(props.mode)
     })
 
@@ -21,34 +31,43 @@ const Memory = (props) => {
                 <div>
                     <h3>Click on the Map to create a memory</h3>
                     <h4>My Memories:</h4>
-                    <List>
-                        {props.memories.map((memory) => (
-                            <ListItem button onClick={()=> {props.setGotoMem(memory.id)}}>{memory.name}</ListItem>
-                        ))}
-                    </List>
+                    {/* <div className={classes.root}>
+                        <FixedSizeList height={400} width={300} itemSize={46}>
+                            {props.memories.map((memory) => (
+                                <ListItem button onClick={() => { props.setGotoMem(memory.id) }} style={} key={memory.id}>
+                                    <ListItemText primary={memory.name} />
+                                </ListItem>
+                                // <ListItem button onClick={() => { props.setGotoMem(memory.id) }}>{memory.name}</ListItem>
+                            ))}
+                        </FixedSizeList>
+                    </div> */}
                 </div>
             )
             break
         case "view":
+            if(props.memories.find(e => e.id === props.viewMem)){
             content = (
                 <div>
-                    <ViewMemory mem={props.memories.find(e=> e.id === props.viewMem)}/>
-                    <Button style={{margin: '5px'}} variant="contained" color="primary" onClick={()=> {setMode("edit")}} endIcon={<Icon>edit</Icon>}>Edit Memory</Button>
-                    <Button style={{margin: '5px'}} variant="contained" color="primary" onClick={()=> {props.deleteMem(props.viewMem)}} endIcon={<Icon>delete</Icon>}>Delete Memory</Button>
-                </div>               
-            )
+                    <ViewMemory mem={props.memories.find(e => e.id === props.viewMem)} />
+                    <Button style={{ margin: '5px' }} variant="contained" color="primary" onClick={() => { setMode("edit") }} endIcon={<Icon>edit</Icon>}>Edit Memory</Button>
+                    <Button style={{ margin: '5px' }} variant="contained" color="primary" onClick={() => { props.deleteMem(props.viewMem) }} endIcon={<Icon>delete</Icon>}>Delete Memory</Button>
+                </div>
+            )}else{
+                content = ''
+                setMode('list')
+            }
             break
         case "create":
             content = (
-                <CreateMemory 
+                <CreateMemory
                     newLoc={props.newLoc}
                     authenticated={props.authenticated}
-                    currentUser={props.currentUser}/>
+                    currentUser={props.currentUser} />
             )
             break
         case "edit":
             content = (
-                <EditMemory mem={props.memories.find(e=> e.id === props.viewMem)}/>
+                <EditMemory mem={props.memories.find(e => e.id === props.viewMem)} />
             )
             break
     }
