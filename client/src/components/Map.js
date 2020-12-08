ffa6d641e63b7a91558d0fff1d5ef55228b9c868
ItinerantMonolith <ItinerantMonolith@gmail.com>
2020-12-07 19:51:47 -0500

    change map.js to use react-mapbox-gl

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,59 +1,93 @@
-import React, { useState, useEffect, useRef } from 'react';
-import mapboxgl from 'mapbox-gl'
+import React, { useState, useEffect } from 'react'
+import ReactMapBoxGl, { Layer, Feature, Popup } from 'react-mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
+const MapView = ReactMapBoxGl({
+   accessToken:
+      'pk.eyJ1IjoibWFya2R2IiwiYSI6ImNraWFubmhzbjAxb3IyeWsyODQ2cXBvbmUifQ.huPMP5ZK_GUqsbjHTgXRcw',
+})
 
 function Map(props) {
-    const mapContainer = useRef(null)
+   const [hoverFlag, setHover] = useState(false)
+   const [hasPopup, setPopup] = useState(false)
+   const [popupLoc, setPopupLoc] = useState([0, 0])
 
-    const styles = {
-        width: "90%",
-        height: "calc(80vh - 80px)",  
-        position: "relative",
-        padding: '10px',
-        margin: '0 auto',
-        top:'auto',
-        bottom: 'auto',
-        
+   const styles = {
+      width: '90%',
+      height: 'calc(80vh - 80px)',
+      position: 'relative',
+      padding: '10px',
+      margin: '0 auto',
+      top: 'auto',
+      bottom: 'auto',
+   }
 
-    }
+   useEffect(() => {}, [])
 
-    useEffect(() => {
-        mapboxgl.accessToken = 'pk.eyJ1IjoibWFya2R2IiwiYSI6ImNraWFubmhzbjAxb3IyeWsyODQ2cXBvbmUifQ.huPMP5ZK_GUqsbjHTgXRcw'
-        const map = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [5, 34],
-            zoom: 2
-        })
+   const viewMemory = (e, m) => {
+      console.log ('m', m)
+      const thisFeature = e.target.queryRenderedFeatures(e.point)[0]
+      // console.log(thisFeature)
+      // console.log('fId is', thisFeature.properties.featureId)
+      // call the callback from profiles to view a memory, just give it the thisFeature.properties.featureId
+      console.log ( `props.viewMemory(${thisFeature.properties.featureId})`)
+   }
 
-        map.on('click', function (e) {
-            const newMarker = new mapboxgl.Marker()
-                .setLngLat([e.lngLat.lng, e.lngLat.lat])
-                .addTo(map)
-                .setPopup(new mapboxgl.Popup({ closeButton: false }).setHTML(
-                    `<p>Make a memory here?</p><button id="newYes">Yes</button><button id="newNo">No</button>`
-                ))
-                .togglePopup()
-            console.log(e.lngLat)
-            const yesButton = document.querySelector('#newYes')
-            yesButton.addEventListener('click', () => {
-                props.history.push(`/createmem/${props.currentUser.id}`)
-            })
-            const noButton = document.querySelector('#newNo')
-            noButton.addEventListener('click', () => {
-                console.log('no')
-                newMarker.remove()
-            })
-        })
-    }, [])
+   const enterExit = () => {
+      setHover(!hoverFlag)
+   }
 
+   const createMemory = () => {
+      setPopup(false)
+      // call the callback from profiles to create memories with popupLoc as the location
+      console.log ( `props.createMemory(${popupLoc})`)
+   }
 
-    return (
-        <div id='map'>
+   const handleMapClick = (map, event) => {
+      if (hoverFlag) return
 
-            <div ref={el => mapContainer.current = el} style={styles} />
-        </div>
-    )
+      if (hasPopup) {
+         setPopup(false)
+         return
+      }
+      setPopupLoc(event.lngLat)
+      setPopup(true)
+   }
+
+   return (
+      <div id="map">
+         <MapView
+            style="mapbox://styles/mapbox/dark-v10"
+            containerStyle={styles}
+            onClick={handleMapClick}
+         >
+            <Layer
+               type="symbol"
+               id="marker"
+               layout={{ 'icon-image': 'marker-15' }}
+            >
+               {props.memories.map((e) => (
+                  <Feature
+                     key={e.id}
+                     coordinates={[e.location.lng, e.location.lat]}
+                     onClick={viewMemory}
+                     onMouseEnter={enterExit}
+                     onMouseLeave={enterExit}
+                     properties={{ featureId: e.id }}
+                  />
+               ))}
+            </Layer>
+            {hasPopup ? (
+               <Popup coordinates={popupLoc}>
+                  <div>
+                     <div>Do you want to create a new Memory?</div>
+                     <button onClick={createMemory}>Yes!</button>
+                  </div>
+               </Popup>
+            ) : null}
+         </MapView>
+      </div>
+   )
 }
 
 export default Map
