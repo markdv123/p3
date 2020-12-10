2aaa23d224d0a8ed06dbc5f956d9580e7183855d
Mark DeVito <markdv@alumni.unc.edu>
2020-12-10 11:06:33 -0500

    fixed error in image model

--- server/models/images.js
+++ server/models/images.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Location.belongsTo( models.Memory, {
+      Image.belongsTo( models.Memory, {
          foreignKey: 'memory_id'
       })
    }
