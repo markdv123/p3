4dd7be1cce6d6a75255ab59c0c4c6e219c5f6fc1
Mark DeVito <markdv@alumni.unc.edu>
2020-12-11 10:42:08 -0500

    deploy againnn

--- server/config/config.js
+++ server/config/config.js
@@ -1,21 +1,26 @@
 require('dotenv').config()
 module.exports = {
-  "development": {
-    "database": "map_dev",
-    "host": "127.0.0.1",
-    "dialect": "postgres",
-    "define": {
+  development: {
+    database: "map_dev",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    define: {
       "underscored": true
     }
   },
-  "test": {
-    "database": "map_test",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  test: {
+    database: "map_test",
+    host: "127.0.0.1",
+    dialect: "postgres"
   },
-  "production": {
-    "database": "map_prod",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+        require: true
+      }
+    }
   }
 }
