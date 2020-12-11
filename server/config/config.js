b5dbb0ddf6c378dd5d406d338a811868154d98e3
Mark DeVito <markdv@alumni.unc.edu>
2020-12-11 15:58:42 -0500

    deploying pt one million

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,9 @@ module.exports = {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    define: {
+      "underscored": true
+    },
     dialectOptions: {
       ssl: {
         rejectUnauthorized: false,
